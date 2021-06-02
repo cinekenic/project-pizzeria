68e1d9df3211a3d3bf95025d3cffd891d25c9c07
Piotr-Trzcinski <cinekenic@go2.pl>
2021-06-02 14:35:29 +0200

    add initOrderForm and processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,10 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log("new Product:", thisProduct);
     }
     renderInMenu() {
@@ -75,19 +78,38 @@
       //add element to menu
       menuContainer.appendChild(thisProduct.element);
     }
+
+    getElements() {
+      const thisProduct = this;
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
+      thisProduct.form = thisProduct.element.querySelector(
+        select.menuProduct.form
+      );
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(
+        select.all.input
+      );
+      thisProduct.cartButton = thisProduct.element.querySelector(
+        select.menuProduct.cartButton
+      );
+      thisProduct.priceElem = thisProduct.element.querySelector(
+        select.menuProduct.priceElem
+      );
+    }
     initAccordion() {
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      let clickableTrigger = document.querySelectorAll(
-        select.menuProduct.clickable
-      );
+      // let clickableTrigger = document.querySelectorAll(
+      //   select.menuProduct.clickable
+      // );
 
-      for (let el of clickableTrigger) {
-        clickableTrigger = el;
-      }
+      // for (let el of clickableTrigger) {
+      //   clickableTrigger = el;
+      // }
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener("click", function (event) {
+      thisProduct.accordionTrigger.addEventListener("click", function (event) {
         /* prevent default action for event */
         event.preventDefault();
 
@@ -102,6 +124,27 @@
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle("active");
       });
+    }
+    initOrderForm() {
+      const thisProduct = this;
+      thisProduct.form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener("change", function () {
+          thisProduct.processOrder();
+        });
+      }
+      thisProduct.cartButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder() {
+      const thisProduct = this;
+      console.log("processOrder");
     }
   }
 

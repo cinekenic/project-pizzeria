3b87c6a104b154ffd163686778c7ef7b614641c6
Piotr-Trzcinski <cinekenic@go2.pl>
2021-06-07 14:02:28 +0200

    add images

--- src/js/script.js
+++ src/js/script.js
@@ -96,6 +96,9 @@
       thisProduct.priceElem = thisProduct.element.querySelector(
         select.menuProduct.priceElem
       );
+      thisProduct.imageWrapper = thisProduct.element.querySelector(
+        select.menuProduct.imageWrapper
+      );
     }
     initAccordion() {
       const thisProduct = this;
@@ -115,14 +118,15 @@
 
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(".active");
-        console.log(activeProduct);
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (activeProduct == thisProduct) {
-          activeProduct.classList.remove("active");
-        }
 
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct != null && activeProduct != thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
         /* toggle active class on thisProduct.element */
-        thisProduct.element.classList.toggle("active");
+        thisProduct.element.classList.toggle(
+          classNames.menuProduct.wrapperActive
+        );
       });
     }
     initOrderForm() {
@@ -137,6 +141,7 @@
           thisProduct.processOrder();
         });
       }
+
       thisProduct.cartButton.addEventListener("click", function (event) {
         event.preventDefault();
         thisProduct.processOrder();
@@ -147,7 +152,6 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log("formData", formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -156,28 +160,36 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+
           // check if there is param with a name of paramId in formData and if it includes optionId
-          console.log(option.price);
-          console.log(thisProduct.priceElem);
-          console.log(price);
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+
+          const optionImage = thisProduct.imageWrapper.querySelector(
+            `.${paramId}-${optionId}`
+          );
+
+          const optionSelected =
+            formData[paramId] && formData[paramId].includes(optionId);
+
+          if (optionSelected) {
             // check if the option is not default
-            if (option.default !== true) {
-              // add option price to price variable
-              price = price + option.price;
+            if (!option.default == true) {
+              price += option.price;
             }
-          } else {
-            // check if the option is default
-            if (option) {
-              // reduce price variable
-              price = price - option.price;
+          } else if (option.default == true) {
+            price -= option.price;
+          }
+
+          if (optionImage) {
+            if (optionSelected) {
+              optionImage.classList.add("active");
+              // classNames.menuProduct.imageVisible;
+            } else {
+              optionImage.classList.remove("active");
             }
           }
         }
@@ -191,7 +203,6 @@
   const app = {
     initMenu() {
       const thisApp = this;
-      console.log("thisApp.data:", thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);

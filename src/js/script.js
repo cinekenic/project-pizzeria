fe4ea1fec6c34d81a192510d6314e8d4f22a1e2d
Piotr-Trzcinski <cinekenic@go2.pl>
2021-06-08 23:25:46 +0200

    show and hide cart

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
   const select = {
     templateOf: {
       menuProduct: "#template-menu-product",
+      cartProduct: "#template-cart-product", // CODE ADDED
     },
     containerOf: {
       menu: "#product-list",
@@ -26,11 +27,32 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: "input.amount", // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+      productList: ".cart__order-summary",
+      toggleTrigger: ".cart__summary",
+      totalNumber: `.cart__total-number`,
+      totalPrice:
+        ".cart__total-price strong, .cart__order-total .cart__order-price-sum strong",
+      subtotalPrice: ".cart__order-subtotal .cart__order-price-sum strong",
+      deliveryFee: ".cart__order-delivery .cart__order-price-sum strong",
+      form: ".cart__order",
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: ".widget-amount",
+      price: ".cart__product-price",
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
+    // CODE ADDED END
   };
 
   const classNames = {
@@ -38,6 +60,11 @@
       wrapperActive: "active",
       imageVisible: "active",
     },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: "active",
+    },
+    // CODE ADDED END
   };
 
   const settings = {
@@ -45,13 +72,23 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
+    }, // CODE CHANGED
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20,
     },
+    // CODE ADDED END
   };
 
   const templates = {
     menuProduct: Handlebars.compile(
       document.querySelector(select.templateOf.menuProduct).innerHTML
     ),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.cartProduct).innerHTML
+    ),
+    // CODE ADDED END
   };
 
   class Product {
@@ -273,6 +310,30 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCard = this;
+      thisCard.products = [];
+      thisCard.getElements(element);
+      thisCard.initAction();
+      console.log("new card", thisCard);
+    }
+    getElements(element) {
+      const thisCard = this;
+      thisCard.dom = {};
+      thisCard.dom.wrapper = element;
+      thisCard.dom.toggleTrigger = thisCard.dom.wrapper.querySelector(
+        select.cart.toggleTrigger
+      );
+    }
+    initAction() {
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener("click", function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu() {
       const thisApp = this;
@@ -298,7 +359,13 @@
       thisApp.initData();
       thisApp.initMenu();
     },
+    initCard: function () {
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
   };
 
   app.init();
+  app.initCard();
 }
